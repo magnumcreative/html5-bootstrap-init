197b7592bd7c28a90569f692d6d26af71d728163
Malcolm Ellis <malcolm@Malcolms-iMac.local>
2014-08-13 16:44:36 -0700

    Gruntfile will now create the default directory structure for css/js/less/src files. The defaultDirectories can be modified in the package.json file

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
 	        },
 	        files: {
 	          // target.css file: source.less file
-	          "css/main.css": "less/main.less"
+	          "css/<%= pkg.name %>.css": "less/<%= pkg.name %>.less"
 	        }
 	      }
     	},
@@ -40,9 +40,33 @@ module.exports = function(grunt) {
 		    },
 		},
 	});
+	// Create custom directories - directories are set in package.json
+	grunt.registerTask('setup', 'Setting up default directories', function() {
+	  var p = grunt.file.readJSON('package.json');
+	  var less_file_exists = grunt.file.exists(p.defautDirectories['less-dir']+"/"+ p.name+'.less');
+	  var css_file_exists = grunt.file.exists(p.defautDirectories['css-dir']+"/"+p.name+'.css');
+	  var src_file_exists = grunt.file.exists(p.defautDirectories['src-dir']+"/"+ p.name+'.js');
+	  var js_file_exists = grunt.file.exists(p.defautDirectories['js-dir']+"/"+ p.name+'.min.js');
+      if(!css_file_exists){
+      	grunt.file.write(p.defautDirectories['css-dir']+"/"+ p.name+'.css',"");
+      	grunt.log.writeln('Created file: '+p.defautDirectories['css-dir']+"/"+ p.name+'.css');
+      }
+	  if(!less_file_exists){
+	  	grunt.file.write(p.defautDirectories['less-dir']+"/"+ p.name+'.less',"");
+	  	grunt.log.writeln('Created file: '+p.defautDirectories['less-dir']+"/"+ p.name+'.less');
+	  }
+	  if(!src_file_exists){
+	  	grunt.file.write(p.defautDirectories['src-dir']+"/"+ p.name+'.js',"");
+	  	grunt.log.writeln('Created file: '+p.defautDirectories['src-dir']+"/"+ p.name+'.js');
+	  }
+	  if(!js_file_exists){
+	  	grunt.file.write(p.defautDirectories['js-dir']+"/"+ p.name+'.min.js',"");
+	  	grunt.log.writeln('Created file: '+p.defautDirectories['js-dir']+"/"+ p.name+'.min.js');
+	  }
+	});
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.registerTask('default', ['uglify' , 'watch', 'jshint', 'less']);
+	grunt.registerTask('default', ['setup','uglify' , 'jshint', 'less','watch']);
 };
--- README.md
+++ README.md
@@ -5,7 +5,8 @@ This is a boilerplate Gruntfile.js and package.json file for use in new projects
 
 It includes Grunt plugins: uglify, watch, jshint, less
 
-Assumes you have a "/src" dir for you development js and a "/js" dir for production
-Assumes you have a "/less" dir for development and a "/css" dir for production
+Create a "/src" dir for you development files and a "/js" dir for production files
 
-The above directories can be modified in the packages.json if required. 
+Create a "/less" dir for development files and a "/css" dir for production files
+
+The above directories can be modified in the package.json if required. 
